3cf857ec8c73ebd8629d32bf130224e40fb1fa61
Edward <llerandi@users.noreply.github.com>
2026-05-11 22:09:17 +0200

    feat(producer): spring initializr project

---
+++ airbnb-producer/src/main/java/dev/llerandi/airbnb_producer/AirbnbProducerApplication.java
@@ -0,0 +1,13 @@
+package dev.llerandi.airbnb_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbProducerApplication.class, args);
+	}
+
+}
---
+++ airbnb-producer/src/test/java/dev/llerandi/airbnb_producer/AirbnbProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.llerandi.airbnb_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
